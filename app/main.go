eed556ae61c9b15900df42eed73088c966030434
Jordan Fox <jordan.fox@canstar.com.au>
2023-10-26 09:12:45 +1000

    created web server

---
+++ app/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func newRouter() *httprouter.Router {
+	mux := httprouter.New()
+	mux.GET("/youtube/channel/stats", getChannelStats())
+
+	return mux
+}
+
+func getChannelStats() httprouter.Handle {
+	// return a function that matches the .Handle signature
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		w.Write([]byte("response!"))
+	}
+}
+
+func main() {
+	srv := &http.Server{
+		Addr:    ":10101",
+		Handler: newRouter(),
+	}
+
+	idleConnsClosed := make(chan struct{})
+	go func() {
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt)
+		signal.Notify(sigint, syscall.SIGTERM)
+		<-sigint
+
+		log.Println("Service interrupt received")
+
+		ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+		defer cancel()
+
+		if err := srv.Shutdown(ctx); err != nil {
+			log.Printf("http server shutdown error: %v", err)
+		}
+
+		log.Print("shutdown complete")
+		close(idleConnsClosed)
+
+	}()
+
+	log.Printf("starting server on port 10101")
+	if err := srv.ListenAndServe(); err != nil {
+		if !errors.Is(err, http.ErrServerClosed) {
+			log.Fatalf("fatal https server failed to start: %v", err)
+		}
+	}
+
+	<-idleConnsClosed
+	log.Println("service stop")
+}
